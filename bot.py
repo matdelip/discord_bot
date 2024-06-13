ea5e140ea552ea4dc9035e0aa11bdfbeeb72c30a
matdelip <matdeli.parra@seattlecolleges.edu>
2024-06-13 00:22:33 -0700

    push

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import random
 from ec2_metadata import ec2_metadata
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('test.env')
 
 token = str(os.getenv('TOKEN'))
 
@@ -15,8 +15,8 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-print('This is my Ec2_metadata.region:', ec2_metadata.region)
-print('This is my Ec2_metadata.instance.id:', ec2_metadata.instance_id)
+#print('This is my Ec2_metadata.region:', ec2_metadata.region)
+#print('This is my Ec2_metadata.instance.id:', ec2_metadata.instance_id)
 
 @client.event
 async def on_ready():
